6e38753e6a4cf7281632fdf828489fff6626b0c6
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2019-10-09 11:57:51 -0400

    Add files via upload

--- Chapter-18/NFLregression.py
+++ Chapter-18/NFLregression.py
@@ -10,6 +10,20 @@ from statsmodels.formula.api import ols
 
 nfldata = pd.read_csv("NFLdata.csv")
 
+# full regression model
 nflmodel = ols('Margin~RETTD+PENDIF+PY_A+DPY_A+RY_A+DRY_A+TO+DTO-1',data = nfldata).fit()
+print(nflmodel.summary())
 
-nflmodel.summary()
+# passing-only regression
+nflmodelpassing = ols('Margin~PY_A+DPY_A',data = nfldata).fit()
+print(nflmodelpassing.summary())
+
+# rushing-only regression
+nflmodelrushing = ols('Margin~RY_A+DRY_A',data=nfldata).fit()
+print(nflmodelrushing.summary())
+
+# correlation matrix
+print("++++++++++++++++++++++ Correlation Matrix ++++++++++++++++++++++")
+corr = nfldata.drop(columns = ['Year','Margin']).corr()
+
+print(corr)
